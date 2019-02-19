5d7db62a0e880b1e1361a6e2168766c22841f5ab
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-02-19 11:53:38 -0500

    debugging passport error: getting a "Cannot read property email of undefined" whenever attempting post request through postman

--- controllers/users.js
+++ controllers/users.js
@@ -6,5 +6,36 @@ const config = require('../config/config')
 const mongoose = require('../models/User')
 const User = mongoose.model('User')
 
+router.post('/signup', (req, res) => {
+  if (req.body.email && req.body.password) {
+    let newUser = {
+      email: req.body.email,
+      password: req.body.password
+    }
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          User.create(newUser)
+            .then(user => {
+              if (user) {
+                var payload = {
+                  id: newUser.id
+                }
+                var token = jwt.encode(payload, config.jwtSecret)
+                res.json({
+                  token: token
+                })
+              } else {
+                res.sendStatus(401)
+              }
+            })
+        } else {
+          res.sendStatus(401)
+        }
+      })
+  } else {
+    res.sendStatus(401)
+  }
+})
 
 module.exports = router
