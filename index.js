349d4bf5cc0953e83fffab6b12df4ef361d417ad
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-02-22 10:48:30 -0500

    changed index

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const userController = require('./controllers/users.js')
 const eventController = require('./controllers/event')
 const cors = require('cors')
 
-let port = process.env.PORT || 3001
+app.set('port', process.env.PORT || 3001)
 
 app.use(cors())
 app.use(parser.json())
 app.use(passport.initialize())
 app.use('/users', userController)
 app.use('/events', eventController)
-app.listen(port, () => console.log('Listening on port 3001 :)'))
+app.listen(app.get('port'), () => console.log('Listening :)'))
