8c327eea6271e38974ccf90627c168f22bc82e5c
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-02-22 09:40:44 -0500

    added headers and changed post in index.js

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ const userController = require('./controllers/users.js')
 const eventController = require('./controllers/event')
 const cors = require('cors')
 
+let port = process.env.NODE_ENV || 3001
+
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+  next()
+})
 app.use(parser.json())
 app.use(passport.initialize())
 app.use('/users', userController)
 app.use('/events', eventController)
-app.listen(3001, () => console.log('Listening on port 3001 :)'))
+app.listen(port, () => console.log('Listening on port 3001 :)'))
